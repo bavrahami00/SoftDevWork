fb6ff81c0f3d059660df003484e35ea14fb788e9
bavrahami00 <43038755+bavrahami00@users.noreply.github.com>
2019-09-16 20:41:02 -0400

    Created a dictionary from the csv file

--- 03_csv/occupations.py
+++ 03_csv/occupations.py
@@ -1,1 +1,14 @@
+import csv
+
+file = "occupations.csv"
+
+ref = {}
+
+with open(file, 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  reader.next()
+  for row in reader:
+    ref[row[0]] = row[1]
+    
+file.close()
 
