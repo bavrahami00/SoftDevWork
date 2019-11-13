1fa283c932a063475c12b58504040080e40556a5
bavrahami00 <bavrahami00@stuy.edu>
2019-11-13 16:13:00 -0500

    Changed dictionary calls, updated urllib calls

--- 25_restrio/mapapi.py
+++ 25_restrio/mapapi.py
@@ -5,15 +5,18 @@
 
 from flask import Flask
 from flask import render_template
-import urllib, json
+import urllib3
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib.request.urlopen("http://open.mapquestapi.com/geocoding/v1/address?key=KHiNxiOAxR7QXpKrSDHZ3sGXP65Ci4Mz&location=345+Chambers+St,New+York,NY,10282")
-    response = u.read()
+    http = urllib3.PoolManager()
+    response = http.request('GET', "http://open.mapquestapi.com/geocoding/v1/address?key=KHiNxiOAxR7QXpKrSDHZ3sGXP65Ci4Mz&location=345+Chambers+St,New+York,NY,10282")
+    # u = urllib.request.urlopen("http://open.mapquestapi.com/geocoding/v1/address?key=KHiNxiOAxR7QXpKrSDHZ3sGXP65Ci4Mz&location=345+Chambers+St,New+York,NY,10282")
+    # response = u.read()
     data = json.loads(response)
-    return render_template("imapapi.html", place=data['street'], lat=data['lat'], long=data['lng'])
+    return render_template("mapapi.html", place=data['results'][0]['providedLocation']['location'], lat=data['results'][0]['locations']['latLng']['lat'], long=data['results'][0]['locations']['latLng']['lng'])
 
 if __name__ == "__main__":
     app.debug = True
