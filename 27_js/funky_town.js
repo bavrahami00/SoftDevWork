7e61caaaeb13d0c44e3032b180f868bd570aa76d
Benjamin Avrahami <43038755+bavrahami00@users.noreply.github.com>
2019-12-11 15:52:40 -0500

    Made js same as partner

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,23 +1,45 @@
-var fibonacci = function(n) {
-  if (n == 0) {
-    return 0;
-  }
-  if (n == 1) {
+var fact = function(n) {
+	if (n == 1){
     return 1;
   }
   else {
-    return fibonacci(n-1)+fibonacci(n-2);
+    return n * (fact(n - 1));
   }
-}
+};
 
-var gcd = function(a,b) {
-  var ans = 0;
-  for (x = 1; x <= a; x++) {
-    if (a%x == 0) {
-      if (b/x == 0) {
-        ans = x;
-      }
-    }
+var fibonacci = function(n) {
+  var first = 0;
+  var second = 1;
+  while (n > 0){
+    var temp = first;
+    first = second;
+    second = temp + second;
+    n -= 1;
   }
-  return ans;
-}
+  return first;
+};
+
+var gcd = function(a, b) {
+  var dend, dsor, r;
+  if (a > b){
+    dend = a;
+    dsor = b;
+    r = a % b;
+  }
+  else{
+    dend = b;
+    dsor = a;
+    r = b % a;
+  }
+  while (r > 0){
+    dend = dsor;
+    dsor = r;
+    r = dend % dsor;
+  }
+  return dsor;
+};
+
+var randomStudent = function() {
+  var list = ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'];
+  return list[Math.floor(Math.random() * list.length)];
+};
