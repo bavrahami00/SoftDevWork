92d03c45c4f7bf511ba0efe2c6ae3bfab56d038d
bavrahami00 <bavrahami00@stuy.edu>
2019-12-13 09:32:05 -0500

    Most of the way (need DP of fib)

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,18 +1,60 @@
 var changeHeading = function(e) {
   var h = document.getElementById("h");
-  h.innerHTML = e.innerHTML;
+  h.innerHTML = this.innerHTML;
+  //console.log(h.innerHTML);
 };
 
 var removeItem = function(e) {
-  e.remove();
+  this.remove();
 }
 
 var lis = document.getElementsByTagName("li");
 
 for (var i = 0; i < lis.length; i++) {
-  console.log(lis[i]);
-  console.log("Length: " + lis.length);
-  lis[i].addEventListener('mouseover', changeHeading(lis[i]));
-  lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World";});
-  lis[i].addEventListener('click', removeItem(lis[i]));
+  //console.log(lis[i]);
+  //console.log("Length: " + lis.length);
+  lis[i].addEventListener('mouseover', changeHeading);
+  lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!";});
+  lis[i].addEventListener('click', removeItem);
 }
+
+var addItem = function(e) {
+  var list = document.getElementById("thelist");
+  var item = document.createElement("li");
+  item.innerHTML = "WORD";
+  item.addEventListener('mouseover', changeHeading);
+  item.addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!";});
+  item.addEventListener('click', removeItem);
+  list.append(item);
+}
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem);
+
+var fib = function(n) {
+  console.log(n);
+  if (n < 2) {
+    return 1;
+  }
+  else {
+    return fib(n-1) + fib(n-2);
+  }
+}
+
+var addFib = function(e) {
+  console.log(e);
+  var list = document.getElementById("fiblist");
+  //console.log(list);
+  var item = document.createElement("li");
+  //item.innerHTML = fib(list.childNodes.length-1);
+  item.innerHTML = addFib2;
+  list.append(item);
+}
+
+var addFib2 = function(e) {
+  console.log(e);
+
+}
+
+var fb = document.getElementById("fb");
+fb.addEventListener('click', addFib);
