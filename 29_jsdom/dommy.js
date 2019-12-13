d612f546303b60a356be69c6bda71829125db0db
bavrahami00 <bavrahami00@stuy.edu>
2019-12-13 09:01:19 -0500

    Imported previous work

---
+++ 29_jsdom/dommy.js
@@ -0,0 +1,18 @@
+var changeHeading = function(e) {
+  var h = document.getElementById("h");
+  h.innerHTML = e.innerHTML;
+};
+
+var removeItem = function(e) {
+  e.remove();
+}
+
+var lis = document.getElementsByTagName("li");
+
+for (var i = 0; i < lis.length; i++) {
+  console.log(lis[i]);
+  console.log("Length: " + lis.length);
+  lis[i].addEventListener('mouseover', changeHeading(lis[i]));
+  lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World";});
+  lis[i].addEventListener('click', removeItem(lis[i]));
+}
