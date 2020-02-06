07de756165aa162b9606d2f3f3f3f74759ff8b3b
Benjamin Avrahami <43038755+bavrahami00@users.noreply.github.com>
2020-02-06 14:16:27 -0500

    Copied over partner's file

---
+++ 04_canvas/clickdraw.js
@@ -0,0 +1,60 @@
+// retrieve node in DOM via ID
+var c = document.getElementById("slate");
+
+// instantiate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+// setting Mona Lisa as canvas for ART
+var imgPath = "monalisa.jpg";
+var imgObj = new Image();
+imgObj.src = imgPath;
+imgObj.onload = function() {
+    ctx.drawImage(imgObj, 65, 0);
+}
+console.log("Masterpiece completed.")
+
+// current drawing mode
+var currentMode = document.getElementById("mode");
+
+// draw function to draw box or dot depending on mode
+var draw = function(e) {
+    if (currentMode.innerHTML == "Draw a Box") {
+        ctx.fillStyle = "#FF00FF";
+        ctx.fillRect(e.clientX, e.clientY, 33, 33);
+    }
+    else {
+        ctx.fillStyle = "#8B008B";
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 20, 0, 2 * Math.PI, true);
+        ctx.fill();
+    }
+}
+
+// add event listener to canvas
+c.addEventListener("click", draw);
+
+// toggle drawing mode function
+var toggleMode = function(e) {
+    if (currentMode.innerHTML == "Draw a Box") {
+        currentMode.innerHTML = "Draw a Dot";
+        console.log("Changed drawing mode to: Draw a Dot.")
+    }
+    else {
+        currentMode.innerHTML = "Draw a Box";
+        console.log("Changed drawing mode to: Draw a Box.")
+    }
+}
+
+// event listener to toggle drawing mode
+var toggleButton = document.getElementById("toggle");
+toggleButton.addEventListener("click", toggleMode);
+
+// function to clear the canvas today
+var clear = function(e) {
+    ctx.clearRect(0, 0, c.width, c.height);
+    console.log("You destroyed the Mona Lisa!");
+}
+
+// event listener for clearing canvas
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clear);
