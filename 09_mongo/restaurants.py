6c641f821a7ef7232c397c6cc8dd927db0b459f4
bavrahami00 <bavrahami00@stuy.edu>
2020-02-27 14:59:18 -0500

    Nothing works

--- 09_mongo/restaurants.py
+++ 09_mongo/restaurants.py
@@ -1,5 +1,14 @@
 import json
 
-with open('primer-dataset.json') as f:
-    data = [json.loads(line) for line in open('primer-dataset.json', 'r')]
-    print(data)
+from pymongo import MongoClient
+client = MongoClient()
+db = client["restaurants"]
+col = db["inventory"]
+
+data = {}
+with open('primer-dataset.json','r') as f:
+    data = f.read().replace("$date", "date")
+    col.insert_many(data)
+
+#def find_by_borough(bor):
+    #print()
