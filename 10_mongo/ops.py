64ac03d1811217a00614f6b1be7af93122b00926
bavrahami00 <bavrahami00@stuy.edu>
2020-03-03 22:25:56 -0500

    Bad work on ops

--- 10_mongo/ops.py
+++ 10_mongo/ops.py
@@ -6,10 +6,16 @@ client = MongoClient()
 db = client["opioids"]
 col = db["inventory"]
 
+if col.count() == 0:
+    with open('ct_od_deaths.json','r') as file:
+        data = json.load(file)
+#        print(type(data["meta"]))
+#        print(type(data["data"]))
+        col.insert_one(data)
+#        col.insert_many(data["data"])
 
-with open('ct_od_deaths.json','r') as file:
-    data = json.load(file)
-#    print(type(data["meta"]))
-#    print(type(data["data"]))
-    col.insert_one(data)
-#    col.insert_many(data["data"])
+#print(col.find({"meta.view.id" : "rybz-nyjw"}, {"data.0.0": 1}))
+for i in col.find({"meta.view.id" : "rybz-nyjw"}, {"_id": 0, "data.0.0": 1}):
+    cursor = i
+    for j in cursor:
+       print(j.0)
