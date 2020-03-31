482f6441aeb209d9d8873e454bcb624877bd7498
bavrahami00 <bavrahami00@stuy.edu>
2020-03-31 18:08:49 -0400

    Updated 13's work

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -3,31 +3,38 @@ var btn = document.getElementById("clear");
 
 
 var addCircle = function(e) {
-	if (e.originalTarget.id == "vimage") {
-		var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		c.setAttribute( "cx", e.offsetX);
-		c.setAttribute( "cy", e.offsetY);
-		c.setAttribute( "r", "25");
-		c.setAttribute( "fill", "red");
-		//c.setAttribute( "stroke", "black" );
-		pic.appendChild(c);
-		c.addEventListener("click", changeColor);
-		//c.addEventListener("click", e => {c.remove();});
-	}
+    if (e.target.id == "vimage") {
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute( "cx", e.offsetX);
+	c.setAttribute( "cy", e.offsetY);
+	c.setAttribute( "r", "25");
+	c.setAttribute( "fill", "red");
+	//c.setAttribute( "stroke", "black" );
+	pic.appendChild(c);
+	c.addEventListener("click", changeColor);
+	//c.addEventListener("click", e => {c.remove();});
+    }
 }
 
 var changeColor = function(e) {
-	e.originalTarget.remove();
-	console.log(e.originalTarget);
-	var d = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	d.setAttribute( "cx", e.originalTarget.cx.animVal.value);
-	d.setAttribute( "cy", e.originalTarget.cy.animVal.value);
-	d.setAttribute( "r", "25");
-	d.setAttribute( "fill", "teal");
-	pic.appendChild(d);
-	d.addEventListener("click", e => {d.remove()})
+    const circle = e.target;
+    circle.setAttributeNS(null, "fill", "green");
+    circle.addEventListener("click", randomCircle);
+}
+
+var randomCircle = function(e) {
+    const circle = e.target;
+    circle.remove()
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", Math.random() * 450 + 25)
+    c.setAttribute("cy", Math.random() * 450 + 25)
+    c.setAttribute( "r", "25");
+    c.setAttribute( "fill", "red");
+    //c.setAttribute( "stroke", "black" );
+    pic.appendChild(c);
+    c.addEventListener("click", changeColor);
 }
 
 //pic.appendChild( c );
-pic.addEventListener("click",addCircle)
+pic.addEventListener("click", addCircle)
 btn.addEventListener("click", e => {pic.innerHTML = "";})
