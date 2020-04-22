56bb433d8c675f4eabfed26b85588ccac06e55c1
bavrahami00 <bavrahami00@stuy.edu>
2020-04-22 06:02:17 +0100

    Added comments and heading

--- 18_d3/static/charts.js
+++ 18_d3/static/charts.js
@@ -15,12 +15,12 @@ var render = function(e) {
 	for (var k in dict) {
 		arr.push(dict[k]);
 	}
-	var x = d3.scaleLinear().domain([0,d3.max(arr)]).range([0,width]);
-	var y = d3.scaleBand().domain(d3.range(arr.length)).range([0,20 * arr.length]);
-	var graph = d3.select("svg").attr("width", width).attr("height", y.range()[1]).attr("font-family", "sans-serif").attr("font-size", "10").attr("text-anchor", "end");
+	var x = d3.scaleLinear().domain([0,d3.max(arr)]).range([0,width]); // Creates a conversion between the numbers given in the data and the width I want the graph to have
+	var y = d3.scaleBand().domain(d3.range(arr.length)).range([0,20 * arr.length]); // Allows each element of the bar chart to have the same height (20)
+	var graph = d3.select("svg").attr("width", width).attr("height", y.range()[1]).attr("font-family", "sans-serif").attr("font-size", "10").attr("text-anchor", "end"); // Changes some stylistic aspects of the svg area (height, width, font, and font size)
 	var end = graph.selectAll("g").data(arr).join("g").attr("transform", (d,i) =>  `translate(0,${y(i)})`);
-	end.append("rect").attr("fill","blue").attr("width",x).attr("height",y.bandwidth()-1);
-	end.append("text").attr("fill","white").attr("x", d => x(d) - 3).attr("y", y.bandwidth() / 2).attr("dy", "0.35em").text(d => d);
+	end.append("rect").attr("fill","blue").attr("width",x).attr("height",y.bandwidth()-1); // Sets up each bar of the bar chart (gives it its color, height, and width)
+	end.append("text").attr("fill","white").attr("x", d => x(d) - 3).attr("y", y.bandwidth() / 2).attr("dy", "0.35em").text(d => d); // Places each piece of text in the right location
 	curr = curr + 1;
 }
 
